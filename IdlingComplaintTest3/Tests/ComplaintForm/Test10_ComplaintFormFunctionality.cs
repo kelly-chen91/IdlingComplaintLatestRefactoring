acb7cfeda54dc247b6126d66e5da2932b3d54f3c
Kelly Chen <kchen@dep.nyc.gov>
2023-07-03 10:12:57 -0400

    Added some business validation testing

--- IdlingComplaintTest3/Tests/ComplaintForm/Test10_ComplaintFormFunctionality.cs
+++ IdlingComplaintTest3/Tests/ComplaintForm/Test10_ComplaintFormFunctionality.cs
@@ -42,7 +42,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6,28,2023, 4, 20, 00, true), SLEEPTIMER);
@@ -113,7 +113,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -183,7 +183,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -261,7 +261,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -331,7 +331,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -401,7 +401,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -471,7 +471,7 @@ namespace IdlingComplaints.Tests.ComplaintForm
             /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
             ScrollToZipCode();
 
-            Fill_Associated(false);
+            Fill_Associated(false, false);
 
             /*OCCURRENCE*/
             Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
@@ -531,14 +531,85 @@ namespace IdlingComplaints.Tests.ComplaintForm
 
         }
 
+        [Test]
+        public void SuccessfulFormSubmit_YesPOBox_InFrontOf_NoSchool_YesSummonAffidavit()
+        {
+            /*QUALIFYING CRITERIA*/
+            ClickNoButton();
+            Driver.WaitUntilElementFound(By.CssSelector("input[formcontrolname='idc_associatedlastname']"), 20);
 
-        public void Fill_Associated(bool isPOBox)
+            /*PERSON OR COMPANY ASSOCIATED TO COMPLAINT*/
+            ScrollToZipCode();
+
+            Fill_Associated(true, false);
+
+            /*OCCURRENCE*/
+            Occurrence_FromControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 20, 00, true), SLEEPTIMER);
+            Occurrence_ToControl.SendKeysWithDelay(StringUtilities.SelectDate(6, 28, 2023, 4, 23, 00, true), SLEEPTIMER);
+
+            Fill_OccurrenceAddress(2, 3, false);
+
+            Occurrence_SelectVehicleType(2);
+            Occurrence_LicensePlateControl.SendKeysWithDelay(StringUtilities.GenerateRandomString(7), SLEEPTIMER);
+            Occurrence_SelectLicenseState(1);
+            Occurrence_PastOffenseControl.SendKeysWithDelay("Test", SLEEPTIMER);
+            Occurrence_SecondPastOffenseControl.SendKeysWithDelay("Test", SLEEPTIMER);
+
+            Fill_InFrontOfSchool(false);
+
+            Describe_ContentControl.SendKeysWithDelay("Test", SLEEPTIMER);
+
+            ClickWitnessCheckbox();
+            ClickSubmitNoCorrectionCheckbox();
+            ClickSubmit();
+
+            Driver.WaitUntilElementIsNoLongerFound(By.TagName("mat-spinner"), 60); // loads to next page 
+
+            /*EVIDENCE UPLOAD*/
+
+            var successfulSave = Driver.WaitUntilElementFound(By.TagName("simple-snack-bar"), 20);
+            Assert.IsNotNull(successfulSave);
+            if (!successfulSave.Text.Contains("saved success")) Assert.That(successfulSave.Text.Trim(), Is.EqualTo("This form has been saved successfully."), "Flagged inconsistency on purpose.");
+            Driver.WaitUntilElementIsNoLongerFound(By.TagName("simple-snack-bar"), 20); //message says form is saved
+
+            EvidenceUpload_UploadInput = FILE_IMAGE_PATH;
+            string fileName = Path.GetFileName(FILE_IMAGE_PATH);
+            EvidenceUpload_ClickFilesUploadConfirm();
+            Thread.Sleep(SLEEPTIMER);
+
+            var successfulEvidenceUpload = Driver.WaitUntilElementFound(By.TagName("simple-snack-bar"), 20); // message says evidence have successfully uploaded
+            Assert.IsNotNull(successfulEvidenceUpload);
+            if (!successfulEvidenceUpload.Text.Contains("upload")) Assert.That(successfulEvidenceUpload.Text.Trim(),
+                Is.EqualTo("Successfully uploaded file named: " + fileName + "."), "Flagged inconsistency on purpose.");
+
+            Thread.Sleep(SLEEPTIMER);
+            EvidenceUpload_ClickFilesNext();
+            Driver.WaitUntilElementFound(By.CssSelector("mat-radio-button[value='753720001']"), 30); //waits until the oath affidavit appears
+
+            /*OATH AFFIDAVIT*/
+
+            AppearOATH_ClickYes();
+
+            Driver.WaitUntilElementIsNoLongerFound(By.TagName("mat-spinner"), 30);
+
+            AppearOATH_ClickSubmit();
+
+            Driver.WaitUntilElementIsNoLongerFound(By.TagName("mat-spinner"), 60);
+
+            var successfulSubmit = Driver.WaitUntilElementFound(By.TagName("simple-snack-bar"), 20);
+            if (successfulSubmit != null && !successfulSubmit.Text.Contains("submitted success")) Assert.That(successfulSubmit.Text.Trim(), Is.EqualTo("Complaint has been submitted successfully."), "Flagged inconsistency on purpose.");
+
+        }
+
+        public void Fill_Associated(bool isPOBox, bool invalidAddress)
         {
             Associated_CompanyNameControl.SendKeysWithDelay("Test INC", SLEEPTIMER);
             Associated_SelectState(1);
             if (!isPOBox) Associated_HouseNumberControl.SendKeysWithDelay("98", SLEEPTIMER);
             else Associated_ClickPOBox();
-            Associated_StreetNameControl.SendKeysWithDelay("Mott Street", SLEEPTIMER);
+            string street = "Mott Street";
+            if (invalidAddress) street = "WhoCares Street";
+            Associated_StreetNameControl.SendKeysWithDelay(street, SLEEPTIMER);
             Associated_AptFloorControl.SendKeysWithDelay("4th Fl", SLEEPTIMER);
             Associated_CityControl.SendKeysWithDelay("New York", SLEEPTIMER);
             Associated_ZipCodeControl.SendKeysWithDelay("10013", SLEEPTIMER);
