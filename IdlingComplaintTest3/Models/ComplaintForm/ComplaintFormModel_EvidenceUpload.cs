ad1361069eb7b36c13b2e24a14f0e8b4988d886d
Kelly Chen <kchen@dep.nyc.gov>
2023-07-06 08:46:58 -0400

    Fixed Build errors

--- IdlingComplaintTest3/Models/ComplaintForm/ComplaintFormModel_EvidenceUpload.cs
+++ IdlingComplaintTest3/Models/ComplaintForm/ComplaintFormModel_EvidenceUpload.cs
@@ -15,7 +15,7 @@ namespace IdlingComplaints.Models.ComplaintForm
         //IWebElement EvidenceUpload_PreviousControl => Driver.FindElement(By.CssSelector("button[type='button']"));
         public IWebElement EvidenceUpload_NextControl => Driver.FindElement(By.CssSelector("button[type='submit']"));
 
-        IWebElement EvidenceUpload_UploadConfirmControl => Driver.FindElement(By.XPath("//app-upload/mat-card/mat-card-content/div/div[2]/div[2]/button[1]"));
+        //IWebElement EvidenceUpload_UploadConfirmControl => Driver.FindElement(By.XPath("//app-upload/mat-card/mat-card-content/div/div[2]/div[2]/button[1]"));
 
         public IWebElement EvidenceUpload_UploadConfirmControl => Driver.FindElement(By.XPath("//app-upload/mat-card/mat-card-content/div/div[2]/div[2]/button[1]"));
         
