9baf0e4af0af7e8504f5f82f6eead3df0ebc2a11
Maya Lewis707 <malewis@dep.nyc.gov>
2023-07-12 09:44:29 -0400

    update

--- IdlingComplaintTest3/Tests/Home/Test10_OpenComplaintFunctionality.cs
+++ IdlingComplaintTest3/Tests/Home/Test10_OpenComplaintFunctionality.cs
@@ -121,17 +121,16 @@ namespace IdlingComplaints.Tests.Home
             int index = complaintCount.IndexOf("f") + 2;
             string outOfComplaintAmount = complaintCount.Substring(index);
             int totalComplaintAmount = int.Parse(outOfComplaintAmount); //Taking the listed total amount of complaints and turning into int
-            Console.WriteLine(totalComplaintAmount);
+            //Console.WriteLine(totalComplaintAmount);
 
             string itemsPerPage = Driver.ExtractTextFromXPath("//div[1]/mat-form-field/div/div[1]/div/mat-select/div/div[1]/span/span/text()");
             int divideItemsPerPage = int.Parse(itemsPerPage); //Taking the items per page and turning into int
 
 
-            //decimal calculatedPageCount = (decimal)totalComplaintAmount / divideItemsPerPage; //Dividing by 10 items per page
             int calculatedPageCount = totalComplaintAmount % divideItemsPerPage == 0 ? totalComplaintAmount / divideItemsPerPage : (totalComplaintAmount / divideItemsPerPage) + 1;
 
-            Console.WriteLine("Manual page count is: " + pageCount);
-            Console.WriteLine("Calculated page count is: " + calculatedPageCount);
+            //Console.WriteLine("Manual page count is: " + pageCount);
+            //Console.WriteLine("Calculated page count is: " + calculatedPageCount);
             Assert.That(pageCount, Is.EqualTo(calculatedPageCount));
 
         }
@@ -153,8 +152,8 @@ namespace IdlingComplaints.Tests.Home
             int indexTotal = complaintCount.IndexOf("f") + 2;
             string totalComplaintAmount = complaintCount.Substring(indexTotal);
 
-            Console.WriteLine("Total Complaint Amount: " + totalComplaintAmount);
-            Console.WriteLine("Complaint End Range Number: " + complaintRange);
+            //Console.WriteLine("Total Complaint Amount: " + totalComplaintAmount);
+            //Console.WriteLine("Complaint End Range Number: " + complaintRange);
             Assert.That(complaintRange, Is.EqualTo(totalComplaintAmount));
 
         }
@@ -171,7 +170,7 @@ namespace IdlingComplaints.Tests.Home
             string complaintRange = complaintCount.Split('–')[0];
 
 
-            Console.WriteLine("Complaint End Range Number: " + complaintRange);
+            //Console.WriteLine("Complaint Start Range Number: " + complaintRange);
             Assert.That(complaintRange, Is.EqualTo("1 "));
 
         }
